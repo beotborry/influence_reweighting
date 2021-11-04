51e92e3a775a4fb5e40e0205a1e0bc446bae90fc
beotborry <parkjh9229@naver.com>
2021-11-04 14:02:31 +0900

    fix typo

--- run_training.sh
+++ run_training.sh
@@ -25,6 +25,7 @@ do
 done
 
 for scaler in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
     python main.py --method influence --dataset adult --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 25 --t 1500 --naive_acc 86.03 --naive_vio 3.19 --seed 100
     python main.py --method influence --dataset adult --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 25 --t 1500 --naive_acc 86.08 --naive_vio 3.98 --seed 200
     python main.py --method influence --dataset adult --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 25 --t 1500 --naive_acc 85.93 --naive_vio 4.64 --seed 300
@@ -34,4 +35,3 @@ for scaler in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
     python main.py --method influence --dataset compas --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 5 --t 1000 --naive_acc 69.65 --naive_vio 16.69 --seed 300
     python main.py --method influence --dataset compas --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 5 --t 1000 --naive_acc 69.32 --naive_vio 17.55 --seed 777
 done
-
