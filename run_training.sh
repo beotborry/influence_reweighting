9e98e479bec0e7b36e596aef56f27c262e3816c1
beotborry <parkjh9229@naver.com>
2021-11-04 13:24:26 +0900

    merge shell

--- run_training.sh
+++ run_training.sh
@@ -26,7 +26,6 @@ do
 done
 
 for scaler in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-do
     python main.py --method influence --dataset adult --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 25 --t 1500 --naive_acc 86.03 --naive_vio 3.19 --seed 100
     python main.py --method influence --dataset adult --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 25 --t 1500 --naive_acc 86.08 --naive_vio 3.98 --seed 200
     python main.py --method influence --dataset adult --epoch 20 --iteration 15 --constraint eo --scaler $scaler --r 25 --t 1500 --naive_acc 85.93 --naive_vio 4.64 --seed 300
