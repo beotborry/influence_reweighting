5b5df353920f50a20675188e73d87a2ac1efca14
juhyeon server <parkjh9229@naver.com>
2022-02-03 12:31:46 +0900

    remove missing_training.sh

--- missing_training.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 main_final.py --dataset compas --method naive_leave_bottom_k_out --seed 1 --constraint eopp --epoch 50 --iteration 1 --gpu 0 --k 7.0 --sen_attr sex --fine_tuning 0 --main_option intersect
-
